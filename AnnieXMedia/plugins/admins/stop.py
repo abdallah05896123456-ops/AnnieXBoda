7c25acb6c9b689cde7ca6a4cdad1020b786dd931
as05896123-dotcom <as05896123@gmail.com>
2026-01-11 03:13:35 +0200

    Update stop.py

--- AnnieXMedia/plugins/admins/stop.py
+++ AnnieXMedia/plugins/admins/stop.py
@@ -1,18 +1,19 @@
+# Authored By Certified Coders © 2025
 from pyrogram import filters
 from pyrogram.types import Message
 
-from BrandrdXMusic import app
-from BrandrdXMusic.core.call import Hotty
-from BrandrdXMusic.utils.database import set_loop
-from BrandrdXMusic.utils.decorators import AdminRightsCheck
-from BrandrdXMusic.utils.inline import close_markup
+from AnnieXMedia import app
+from AnnieXMedia.core.call import StreamController
+from AnnieXMedia.utils.database import set_loop
+from AnnieXMedia.utils.decorators import AdminRightsCheck
+from AnnieXMedia.utils.inline import close_markup
 from config import BANNED_USERS
 
 
 @app.on_message(
     filters.command(
-        ["end", "stop", "cend", "cstop", "انهاء", "ايقاف", "وقف", "بس", "اسكت"],
-        prefixes=["/", "!", ".", "", "@", "#"]
+        ["end", "stop", "انهاء", "وقف", "استوب"],  # كل الأوامر المدعومة
+        prefixes=["/", "!", ""]  # بدون اسلاش أو بعلامات / و !
     )
     & filters.group
     & ~BANNED_USERS
@@ -21,7 +22,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await Hotty.stop_stream(chat_id)
+    await StreamController.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
