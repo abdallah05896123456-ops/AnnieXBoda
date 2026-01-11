81fa49aa571262449615bd95bb623d31a696826b
as05896123-dotcom <as05896123@gmail.com>
2026-01-11 10:44:27 +0200

    Update Resso.py

--- AnnieXMedia/platforms/Resso.py
+++ AnnieXMedia/platforms/Resso.py
@@ -1,12 +1,10 @@
 # Authored By Certified Coders © 2025
 import re
-import asyncio
 from typing import Union
 
 import aiohttp
 from bs4 import BeautifulSoup
-# التعديل: استيراد المكتبة العادية
-from youtubesearchpython import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 
 
 class RessoAPI:
@@ -29,8 +27,6 @@ class RessoAPI:
                     return False
                 html = await response.text()
         soup = BeautifulSoup(html, "html.parser")
-        title = None
-        des = ""
         for tag in soup.find_all("meta"):
             if tag.get("property", None) == "og:title":
                 title = tag.get("content", None)
@@ -40,26 +36,15 @@ class RessoAPI:
                     des = des.split("·")[0]
                 except:
                     pass
-        
-        if not title:
+        if des == "":
             return
-
-        # التعديل: البحث في Thread منفصل
-        def _search():
-            return VideosSearch(title, limit=1).result()
-
-        data = await asyncio.to_thread(_search)
-
-        if not data or not data.get("result"):
-            return
-
-        for result in data["result"]:
+        results = VideosSearch(title, limit=1)
+        for result in (await results.next())["result"]:
             title = result["title"]
             ytlink = result["link"]
             vidid = result["id"]
             duration_min = result["duration"]
             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        
         track_details = {
             "title": title,
             "link": ytlink,
