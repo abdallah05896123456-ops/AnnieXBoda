17c47dd4a136fee83fe4651d5aa0d356bce550c9
as05896123-dotcom <as05896123@gmail.com>
2026-01-11 02:49:27 +0200

    Update language.py

--- AnnieXMedia/utils/decorators/language.py
+++ AnnieXMedia/utils/decorators/language.py
@@ -23,7 +23,7 @@ def language(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("ar")
         return await mystic(_, message, language)
 
     return wrapper
@@ -41,7 +41,7 @@ def languageCB(mystic):
             language = await get_lang(CallbackQuery.message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("ar")
         return await mystic(_, CallbackQuery, language)
 
     return wrapper
@@ -53,7 +53,7 @@ def LanguageStart(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("ar")
         return await mystic(_, message, language)
 
     return wrapper
