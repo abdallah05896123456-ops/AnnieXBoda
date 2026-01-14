0ff717da5f7a6d23fa522a2d32dc667a8e135cc1
chatgpt05896-lang <chatgpt05896@gmail.com>
2026-01-14 22:27:02 +0200

    Update __init__.py

--- AnnieXMedia/__init__.py
+++ AnnieXMedia/__init__.py
@@ -1,4 +1,4 @@
-﻿# Authored By Certified Coders © 2025
+# Authored By Certified Coders © 2025
 from AnnieXMedia.core.bot import MusicBotClient
 from AnnieXMedia.core.dir import StorageManager
 from AnnieXMedia.core.git import git
@@ -8,7 +8,7 @@ from AnnieXMedia.misc import dbb, heroku
 from .logging import LOGGER
 
 StorageManager()
-git()
+# git()
 dbb()
 heroku()
 
