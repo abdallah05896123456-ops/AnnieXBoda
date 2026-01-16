c63ccdbaec84dd8a5507f3d7d7a827d95f41ec13
chatgpt05896-lang <chatgpt05896@gmail.com>
2026-01-16 22:28:00 +0200

    Update Apple.py

--- AnnieXMedia/platforms/Apple.py
+++ AnnieXMedia/platforms/Apple.py
@@ -4,8 +4,7 @@ from typing import List, Union, Optional
 
 import aiohttp
 from bs4 import BeautifulSoup
-# التعديل هنا: استخدام __future__ بدل aio
-from youtubesearchpython.__future__ import VideosSearch
+from youtubesearchpython.aio import VideosSearch
 
 
 class AppleAPI:
