03addd1abaf2491f5467692e2ce0be12850887fc
abdallah05896123456-ops <abdallah05896123456@gmail.com>
2026-01-26 14:13:24 +0200

    Update __init__.py

--- AnnieXMedia/__init__.py
+++ AnnieXMedia/__init__.py
@@ -1,7 +1,4 @@
 # Authored By Certified Coders © 2025
-# NUCLEAR EDITION: Local Server Support for 2GB Uploads & RAM Disk
-# [attachment_0](attachment)
-
 from AnnieXMedia.core.bot import MusicBotClient
 from AnnieXMedia.core.dir import StorageManager
 from AnnieXMedia.core.git import git
@@ -10,22 +7,15 @@ from AnnieXMedia.misc import dbb, heroku
 
 from .logging import LOGGER
 
-# 1. تهيئة البنية التحتية (الرام ديسك، الجيت، وقاعدة البيانات)
-# يتم توجيه التحميلات تلقائياً إلى /dev/shm/AnnieDownloads لضمان السرعة
 StorageManager()
 git()
 dbb()
 heroku()
 
-# 2. تشغيل العميل النووي
-# الكلاس MusicBotClient مبرمج الآن لتفعيل الـ Local Server يدوياً
 app = MusicBotClient()
-
-# 3. تشغيل الحساب المساعد (Userbot)
 userbot = Userbot()
 
-# 4. استيراد وتهيئة منصات الميديا
-# ملاحظة: YouTubeAPI الآن مربوط بالـ app الذي يدعم رفع ملفات حتى 2 جيجا
+
 from .platforms import *
 
 Apple = AppleAPI()
@@ -35,5 +25,3 @@ Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
-
-LOGGER(__name__).info("Nuclear Core Initialized: 2GB Uploads Enabled via Local Server.")
