507f94e5b49c8cc635ee576bde26c06749c43fdd
abdallah05896123456-ops <abdallah05896123456@gmail.com>
2026-01-26 06:36:02 +0200

    Update __init__.py

--- AnnieXMedia/__init__.py
+++ AnnieXMedia/__init__.py
@@ -1,4 +1,6 @@
 # Authored By Certified Coders © 2025
+# NUCLEAR EDITION: Local Server Support for 2GB Uploads
+
 from AnnieXMedia.core.bot import MusicBotClient
 from AnnieXMedia.core.dir import StorageManager
 from AnnieXMedia.core.git import git
@@ -7,14 +9,19 @@ from AnnieXMedia.misc import dbb, heroku
 
 from .logging import LOGGER
 
+# تهيئة مديري التخزين والأنظمة الأساسية
 StorageManager()
 git()
 dbb()
 heroku()
 
+# تشغيل البوت مع دعم السيرفر المحلي لرفع ملفات حتى 2 جيجابايت
+# ملاحظة: تأكد من تشغيل telegram-bot-api في الخلفية على منفذ 8081
 app = MusicBotClient()
-userbot = Userbot()
 
+# ربط العميل بالسيرفر المحلي (يتم ضبط هذا غالباً داخل كلاس MusicBotClient)
+# إذا أردت التأكيد البرمجي هنا، تأكد أن الكلاس يدعم تمرير الإعدادات
+userbot = Userbot()
 
 from .platforms import *
 
