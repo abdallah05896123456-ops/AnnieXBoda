c93dd68dfdce418a4b91a9c0efeee47677aa97ff
abdallah05896123456-ops <abdallah05896123456@gmail.com>
2026-01-28 00:32:45 +0200

    Update az_utils.py

--- AnnieXMedia/plugins/AzanSystem/az_utils.py
+++ AnnieXMedia/plugins/AzanSystem/az_utils.py
@@ -10,8 +10,8 @@ from pyrogram import enums
 from pyrogram.errors import FloodWait
 
 # استدعاء مكتبات السورس الأساسية
-from BrandrdXMusic import app
-from BrandrdXMusic.utils.stream.stream import stream
+from AnnieXMedia import app
+from AnnieXMedia.utils.stream.stream import stream
 
 # استدعاء المتغيرات من ملف الإعدادات (Relative Import)
 from .az_conf import (
