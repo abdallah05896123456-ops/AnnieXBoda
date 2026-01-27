9a458590c2c22be3d383d492647b64a37113ba6a
abdallah05896123456-ops <abdallah05896123456@gmail.com>
2026-01-27 22:00:52 +0200

    Update userbot.py

--- AnnieXMedia/core/userbot.py
+++ AnnieXMedia/core/userbot.py
@@ -16,44 +16,38 @@ GROUPS_TO_JOIN = [
     "CertifiedNetwork",
 ]
 
-
-# Initialize userbots
 class Userbot:
     def __init__(self):
+        # 🔥 تم إزالة no_updates=True للسماح للمساعد برؤية المكالمات
         self.one = Client(
             "AnnieAssis1",
             config.API_ID,
             config.API_HASH,
             session_string=str(config.STRING1),
-            no_updates=True,
         )
         self.two = Client(
             "AnnieAssis2",
             config.API_ID,
             config.API_HASH,
             session_string=str(config.STRING2),
-            no_updates=True,
         )
         self.three = Client(
             "AnnieAssis3",
             config.API_ID,
             config.API_HASH,
             session_string=str(config.STRING3),
-            no_updates=True,
         )
         self.four = Client(
             "AnnieAssis4",
             config.API_ID,
             config.API_HASH,
             session_string=str(config.STRING4),
-            no_updates=True,
         )
         self.five = Client(
             "AnnieAssis5",
             config.API_ID,
             config.API_HASH,
             session_string=str(config.STRING5),
-            no_updates=True,
         )
 
     async def start_assistant(self, client: Client, index: int):
