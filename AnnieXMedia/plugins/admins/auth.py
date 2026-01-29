f72d5ce507efa572d709d6819912d74c1f421125
abdallah05896123456-ops <abdallah05896123456@gmail.com>
2026-01-29 22:17:27 +0200

    Update auth.py

--- AnnieXMedia/plugins/admins/auth.py
+++ AnnieXMedia/plugins/admins/auth.py
@@ -1,4 +1,6 @@
-﻿# Authored By Certified Coders © 2025
+# Authored By Certified Coders 2026
+# Module: Auth Users (Bot Admins) - Arabic & No Emojis
+
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -15,18 +17,26 @@ from AnnieXMedia.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
-@app.on_message(filters.command("auth") & filters.group & ~BANNED_USERS)
+# --- [ أمر رفع أدمن في البوت ] ---
+@app.on_message(
+    filters.command(["رفع ادمن", "auth"], prefixes=["", "/", "!", "."]) 
+    & filters.group 
+    & ~BANNED_USERS
+)
 @AdminActual
 async def auth(client, message: Message, _):
     if not message.reply_to_message:
         if len(message.command) != 2:
-            return await message.reply_text(_["general_1"])
+            return await message.reply_text("**يرجى الرد على العضو أو كتابة المعرف لرفعه.**")
+    
     user = await extract_user(message)
     token = await int_to_alpha(user.id)
     _check = await get_authuser_names(message.chat.id)
     count = len(_check)
+    
     if int(count) == 25:
-        return await message.reply_text(_["auth_1"])
+        return await message.reply_text("**لا يمكن رفع المزيد، تم الوصول للحد الأقصى (25 أدمن).**")
+    
     if token not in _check:
         assis = {
             "auth_user_id": user.id,
@@ -39,17 +49,23 @@ async def auth(client, message: Message, _):
             if user.id not in get:
                 get.append(user.id)
         await save_authuser(message.chat.id, token, assis)
-        return await message.reply_text(_["auth_2"].format(user.mention))
+        return await message.reply_text(f"**تم رفع {user.mention} أدمن في البوت بنجاح.**")
     else:
-        return await message.reply_text(_["auth_3"].format(user.mention))
+        return await message.reply_text(f"**{user.mention} أدمن في البوت بالفعل.**")
 
 
-@app.on_message(filters.command("unauth") & filters.group & ~BANNED_USERS)
+# --- [ أمر تنزيل أدمن من البوت ] ---
+@app.on_message(
+    filters.command(["تنزيل ادمن", "unauth"], prefixes=["", "/", "!", "."]) 
+    & filters.group 
+    & ~BANNED_USERS
+)
 @AdminActual
 async def unauthusers(client, message: Message, _):
     if not message.reply_to_message:
         if len(message.command) != 2:
-            return await message.reply_text(_["general_1"])
+            return await message.reply_text("**يرجى الرد على العضو أو كتابة المعرف لتنزيله.**")
+    
     user = await extract_user(message)
     token = await int_to_alpha(user.id)
     deleted = await delete_authuser(message.chat.id, token)
@@ -58,23 +74,26 @@ async def unauthusers(client, message: Message, _):
         if user.id in get:
             get.remove(user.id)
     if deleted:
-        return await message.reply_text(_["auth_4"].format(user.mention))
+        return await message.reply_text(f"**تم تنزيل {user.mention} من صلاحيات البوت.**")
     else:
-        return await message.reply_text(_["auth_5"].format(user.mention))
+        return await message.reply_text(f"**{user.mention} ليس أدمن في البوت أصلاً.**")
 
 
+# --- [ أمر عرض قائمة الادمنية ] ---
 @app.on_message(
-    filters.command(["authlist", "authusers"]) & filters.group & ~BANNED_USERS
+    filters.command(["الادمنية", "authlist", "authusers"], prefixes=["", "/", "!", "."]) 
+    & filters.group 
+    & ~BANNED_USERS
 )
 @language
 async def authusers(client, message: Message, _):
     _wtf = await get_authuser_names(message.chat.id)
     if not _wtf:
-        return await message.reply_text(_["setting_4"])
+        return await message.reply_text("**لا يوجد أدمنية مرفوعين في هذا الجروب.**")
     else:
         j = 0
-        mystic = await message.reply_text(_["auth_6"])
-        text = _["auth_7"].format(message.chat.title)
+        mystic = await message.reply_text("**جاري جلب القائمة...**")
+        text = f"**قائمة أدمنية البوت في {message.chat.title}:**\n\n"
         for umm in _wtf:
             _umm = await get_authuser(message.chat.id, umm)
             user_id = _umm["auth_user_id"]
@@ -85,7 +104,7 @@ async def authusers(client, message: Message, _):
                 j += 1
             except:
                 continue
-            text += f"{j}➤ {user}[<code>{user_id}</code>]\n"
-            text += f"   {_['auth_8']} {admin_name}[<code>{admin_id}</code>]\n\n"
+            text += f"{j}- {user} [`{user_id}`]\n"
+            text += f"   بواسطة: {admin_name} [`{admin_id}`]\n\n"
+        
         await mystic.edit_text(text, reply_markup=close_markup(_))
-    
