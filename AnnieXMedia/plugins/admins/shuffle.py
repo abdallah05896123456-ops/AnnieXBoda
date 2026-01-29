ba86daac887d978b04001c0363e7a40b4d7982c0
abdallah05896123456-ops <abdallah05896123456@gmail.com>
2026-01-29 22:26:44 +0200

    Update shuffle.py

--- AnnieXMedia/plugins/admins/shuffle.py
+++ AnnieXMedia/plugins/admins/shuffle.py
@@ -1,4 +1,6 @@
-﻿# Authored By Certified Coders © 2025
+# Authored By Certified Coders 2026
+# Module: Shuffle Queue - Arabic Commands + Language Support
+
 import random
 
 from pyrogram import filters
@@ -12,23 +14,29 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["shuffle", "cshuffle"]) & filters.group & ~BANNED_USERS
+    filters.command(["shuffle", "cshuffle", "خلط", "عشوائي"], prefixes=["", "/", "!", "."]) 
+    & filters.group 
+    & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def admins(Client, message: Message, _, chat_id):
     check = db.get(chat_id)
     if not check:
         return await message.reply_text(_["queue_2"])
+    
     try:
         popped = check.pop(0)
     except:
         return await message.reply_text(_["admin_15"], reply_markup=close_markup(_))
+    
     check = db.get(chat_id)
     if not check:
         check.insert(0, popped)
         return await message.reply_text(_["admin_15"], reply_markup=close_markup(_))
+    
     random.shuffle(check)
     check.insert(0, popped)
+    
     await message.reply_text(
         _["admin_16"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
