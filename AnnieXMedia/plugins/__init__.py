7ac2347d3543763d1abcacbb8835a91bd169beb7
abdallah05896123456-ops <abdallah05896123456@gmail.com>
2026-01-31 21:44:27 +0200

    Update __init__.py

--- AnnieXMedia/plugins/__init__.py
+++ AnnieXMedia/plugins/__init__.py
@@ -1,11 +1,13 @@
-﻿# Authored By Certified Coders © 2025
+# Authored By Certified Coders © 2025
 import glob
 from os.path import dirname, isfile
 
-
 def __list_all_modules():
     work_dir = dirname(__file__)
+    # البحث في المستوى الأول (مثل tools/file.py)
     mod_paths = glob.glob(work_dir + "/*/*.py")
+    # البحث في المستوى الثاني (مثل tools/games/file.py)
+    mod_paths += glob.glob(work_dir + "/*/*/*.py")
 
     all_modules = [
         (((f.replace(work_dir, "")).replace("/", "."))[:-3])
@@ -14,7 +16,6 @@ def __list_all_modules():
     ]
 
     return all_modules
-
 
 ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
